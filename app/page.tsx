8d493cbfe5cf4abf2ff5103da28842c1648c2c13
Fedekasparian <Federicokasparian@gmail.com>
2025-10-29 02:57:20 -0300

    Update page.tsx
    agregue las secciones a la page

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@ import { Hero } from "@/components/hero"
 import { EventInfo } from "@/components/event-info"
 import { RegistrarForm } from "@/components/registration-form"
 import { Footer } from "@/components/footer"
+import { Schedule } from "@/components/schedule"
+import { Sponsors } from "@/components/sponsors"
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Hero />
       <EventInfo />
+      <Schedule />
       <RegistrarForm />
+      <Sponsors />
       <Footer />
     </main>
   )
